ce2616ce950ee64d2b3c4b8861d2c0e8a21d7caf
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2018-03-27 20:44:09 -0300

    git init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # midpoint-filter
+
+
+This function will filter the image by the nonlinear midpoint filter method. This function works only for monochrome images, 8 bit per bixel and 24 bit per pixel. The midpoint filter is typically used to filter images containing short tail noises such as Gaussian noise and uniform type. The midpoint filter output is the average of the maximum and minimum gray level values ​​within a local region of the image determined by a specified mask.
---
+++ src/midpoint_filter.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+	return 1;
+}
+
+struct Image{
+	int rows;
+	int columns;
+	int data[rows*columns];
+}
+
+
+midpoint(struct Image *IMAGE, struct Image *IMAGE1){
+	int x, y, i, j, smin, smx, n;
+	int a[11][11];
+	n =3;
+	for(y=n/2; y<IMAGE->rows-n/2; y++){
+		for(x=n/2; x<IMAGE->columns-n/2; x++){
+			smin=255; 
+			smax=0;
+			for(j=-n/2; j<n/2; i++){
+				for(i=-n/2; i<n/2; j++){
+					a[i+n/2][j+n/2]=*(IMAGEM->data+x+i+(long)(y+j) * IMAGE->columns);
+				}
+			}
+			for(j=0; j<=n-1;j++){
+				for(i=0; i<=n-1; i++){
+					if(a[i][j] <smin){
+						smin = a[i][j]
+					}
+				}
+			}
+			for(j=0; j<=n-1;j++){
+				for(i=0; i<=n-1; i++){
+					if(a[i][j] > smax){
+						smax = a[i][j]
+					}
+				}
+			}
+			
+			*(IMAGE1->data + x +(long) y *IMAGE->columns) = (smin + smax)/2
+			
+		}
+		
+	}
+	
+}
