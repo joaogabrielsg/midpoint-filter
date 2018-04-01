f15fb1dae4fb2545c8730d9bdd31d62a672a0fbd
Jose Yuri <yurilima.ce@gmail.com>
2018-04-01 19:31:30 -0300

    reading file but read bit by bit of the file

--- src/image.c
+++ src/image.c
@@ -4,36 +4,51 @@
 void image_in(struct Image *IMAGE){
     FILE *file;
     int i;
-
-    file = fopen("teste.bmp", "rb");
-
+    char teste[IMAGE->rows];
+    file = fopen("MYIMAGE.txt", "rb");
     if (file == NULL) {
         printf("Erro na abertura do arquivo de imagem de leitura \n");
     }
+    for(i=0; i<IMAGE->rows; i   ++){
 
-    for(i=0; i<IMAGE->columns; i++){
-        fread(IMAGE->data + i*IMAGE->columns, IMAGE->columns, 1, file);
-        printf("Image in\n");
-        printf("%d\n",&IMAGE->columns);
+         IMAGE->data = fgetc(file);
+         teste[i] = IMAGE->data;
+         //IMAGE->data = fgets(IMAGE->data,IMAGE->rows,file);
+         /*if(IMAGE->data)
+            {
+                //printf("%c",IMAGE->data);
+            }*/
+            //printf("%c",teste[i]);
+
     }
-
+    IMAGE->data = teste;
     fclose(file);
+
+
 }
 
 
 void image_out(struct Image *IMAGE){
     FILE *file;
     int i;
-    file = fopen("out_teste.bmp", "wb");
+    file = fopen("out_teste.txt", "wb");
 
     if (file == NULL) {
         printf("Erro na abertura do arquivo de imagem de escrita \n");
     }
 
-    for(i=0; i<IMAGE->columns; i++){
-        fwrite(IMAGE->data + i*IMAGE->columns, IMAGE->columns, 1, file);
-        printf("Image out \n");
-        printf("%d\n",&IMAGE->columns);
+    for(i=0; i<IMAGE->rows; i++){
+         IMAGE->data = fgets(IMAGE->data,IMAGE->rows,file);
+         if(IMAGE->data)
+            {
+                fputs(IMAGE->data,file);
+            }
+
+
+
+        //fwrite(IMAGE->data + i*IMAGE->columns, IMAGE->columns,1, file);
+        //printf("Image out \n");
+        //printf("%d\n",&IMAGE->columns);
 
     }
 
--- src/main.c
+++ src/main.c
@@ -3,38 +3,16 @@
 #include "image.h"
 
 
-int main()
-{
-
-	struct Image in,out;
-
-
-	in.rows = out.rows = 243;
-	in.columns = out.columns = 243;
-	in.type = out.type = UINT;
-
-	in.data = (unsigned char *)calloc(in.rows,in.columns);
-	out.data = (unsigned char *)calloc(out.rows,out.columns);
-
-	image_in(&in);
-    //midpoint(&in,&out);
-	image_out(&in);
-
-
-
-	return 1;
-}
-
 void midpoint(struct Image *IMAGE, struct Image *IMAGE1){
 	int x, y, i, j, smin, smax, n;
-	int a[3][3];
+	int a[11][11];
 	n =3;
 	for(y=n/2; y<IMAGE->rows-n/2; y++){
 		for(x=n/2; x<IMAGE->columns-n/2; x++){
 			smin=255;
 			smax=0;
-			for(j=-n/2; j<n/2; i++){
-				for(i=-n/2; i<n/2; j++){
+			for(j=-n/2; j<n/2; j++){
+				for(i=-n/2; i<n/2; i++){
 					a[i+n/2][j+n/2]= *(IMAGE->data+x+i+(long)(y+j) * IMAGE->columns);
 				}
 			}
@@ -53,6 +31,40 @@ void midpoint(struct Image *IMAGE, struct Image *IMAGE1){
 				}
 			}
 			*(IMAGE1->data + x +(long) y *IMAGE->columns) = (smin + smax)/2;
+
 		}
 	}
 }
+
+
+
+
+
+
+int main()
+{
+
+	struct Image in,out;
+    int i;
+    char teste;
+
+	in.rows = out.rows = 243;
+	in.columns = out.columns = 243;
+	in.type = out.type = BASIC;
+	in.data = (unsigned char *)calloc(in.rows,in.columns);
+	out.data = (unsigned char *)calloc(out.rows,out.columns);
+	//out.data = (unsigned char *)calloc((out.rows*out.columns),sizeof(unsigned char));
+
+
+	 image_in(&in);
+	 teste = *(in.data +(0*in.columns)+2);
+     printf("%c",teste);
+   // midpoint(&in,&out);
+	//image_out(&out);
+	//printf("%d",out.data);
+
+
+
+	return 1;
+}
+
